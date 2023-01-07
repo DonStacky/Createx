aa8bc9907f5a7423068f76dd52769ba354932d8f
Сергей (DonStacky) <rodnihsergey1994@mail.ru>
2023-01-07 08:58:36 +0300

    feat: add certificate

--- js/main.js
+++ js/main.js
@@ -1,6 +1,14 @@
 $(function () {
-    var mixer = mixitup('.direction__grid');
+    var mixer = mixitup('.direction__grid', {
+        "animation": {
+            enable: false
+        }
+    });
 
+$('.direction__button').on('click', function () {
+    $('.direction__button').removeClass('direction__button--active');
+    $(this).addClass('direction__button--active')
+})
 
 
 
