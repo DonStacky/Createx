bb77edf42777cc9b30d6d8240b180499dc967a27
Сергей (DonStacky) <rodnihsergey1994@mail.ru>
2023-01-08 09:44:40 +0300

    feat: add team slider

--- js/main.js
+++ js/main.js
@@ -10,6 +10,21 @@ $('.direction__button').on('click', function () {
     $(this).addClass('direction__button--active')
 })
 
+$('.team__slider').slick({
+        arrows: false,
+        slidesToShow: 4,
+        draggable: false,
+        waitForAnimate: false
+      })
 
+$('.team__arrow--prev').on('click', function (e) {
+    e.preventDefault()
+    $('.team__slider').slick('slickPrev')
+})
+
+$('.team__arrow--next').on('click', function (e) {
+    e.preventDefault()
+    $('.team__slider').slick('slickNext')
+})
 
 })
